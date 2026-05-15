6016359352aaeb0423e0492c29372710499a6eeb
João Maicon de Oliveira Monte <oliveiramaicon16637@gmail.com>
2026-05-15 20:52:44 -0300

    Refactor: Remove unnecessary comments from UploadVideoRequest struct for cleaner code

--- app/internal/dto/video_dto.go
+++ app/internal/dto/video_dto.go
@@ -7,8 +7,6 @@ import (
 // Inicia o validador para usarmos nas nossas requisições
 var Validate = validator.New()
 
-// UploadVideoRequest representa os dados que esperamos receber do Front-end (Cliente)
-// antes de começar o upload de fato, ou junto com o arquivo.
 type UploadVideoRequest struct {
 	Name        string `json:"name" validate:"required,min=3,max=100"`
 	ContentType string `json:"content_type" validate:"required,oneof=video/mp4 video/x-matroska video/quicktime"` // Aceita mp4, mkv, mov
