8ef2a9e6cd1e9edcf3e443c22a8eee40425683b2
João Maicon de Oliveira Monte <oliveiramaicon16637@gmail.com>
2026-05-16 21:50:13 -0300

    fix: update User struct field tags for database mapping

--- app/internal/models/user.go
+++ app/internal/models/user.go
@@ -7,9 +7,9 @@ import (
 )
 
 type User struct {
-	ID       uuid.UUID `json:"id"`
-	Name	 string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
+ID        uuid.UUID `json:"id" db:"id"`
+Name      string    `json:"name" db:"name"`
+Email     string    `json:"email" db:"email"`
+Password  string    `json:"-" db:"password"`
+CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
